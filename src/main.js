2533c7ae99eebbad0229e2f098e498988fb99a1b
kanagatzhumagaliev <zhumagalievkanagat@gmail.com>
2023-01-25 11:25:13 +0600

    delete: style.css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia';
 import { router } from './router';
 
-@import('@/style/main.scss')
+import('@/style/main.scss')
 
 const app = createApp(App);
 const store = createPinia();
