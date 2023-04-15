f79caab2b6f5fd35244766054cd1477f9904bd96
kanagatzh <kanagatzh@halyklife.kz>
2023-04-15 15:58:48 +0600

    statistic page

--- src/pages/statistics.vue
+++ src/pages/statistics.vue
@@ -38,18 +38,95 @@
         </div>
       </v-col>
     </v-row>
+    <v-row no-gutters class="mt-5">
+      <v-col cols="6" class="pr-3">
+        <div class="basic-border px-6 py-6">
+          <h3 class="chart-title">Денежные поступления</h3>
+          <div class="d-flex align-center">
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">1 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">3 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">6 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn-selected">1 год</v-btn>
+          </div>
+          <LineChart :options="lineChartOptions" :chartData="lineChartData"/>
+        </div>
+      </v-col>
+      <v-col cols="6"  class="pl-2">
+        <div class="basic-border px-6 py-6">
+          <h3 class="chart-title">Количество аренд</h3>
+          <div class="d-flex align-center">
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">1 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">3 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">6 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn-selected">1 год</v-btn>
+          </div>
+          <LineChart :options="lineChartOptions" :chartData="lineChartData"/>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="mt-5">
+      <v-col cols="6" class="pr-3">
+        <div class="basic-border px-6 py-6">
+          <h3 class="chart-title">Количество пользователей</h3>
+          <div class="d-flex align-center">
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">1 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">3 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">6 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn-selected">1 год</v-btn>
+          </div>
+          <LineChart :options="lineChartOptions" :chartData="lineChartData"/>
+        </div>
+      </v-col>
+      <v-col cols="6" class="pl-2">
+        <div class="basic-border px-6 py-6">
+          <h3 class="chart-title">Количество новых пользователей</h3>
+          <div class="d-flex align-center">
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">1 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">3 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn mr-1">6 мес</v-btn>
+            <v-btn elevation="0" class="px-6 w-auto py-1 text-none chart-btn-selected">1 год</v-btn>
+          </div>
+          <LineChart :options="lineChartOptions" :chartData="lineChartData"/>
+        </div>
+      </v-col>
+    </v-row>
+
   </div>
 </template>
 
-<script>
-import Icon from "@/components/icon.vue";
+<script setup>
+import Icon from '@/components/icon.vue';
+import { LineChart } from 'vue-chart-3';
+import { Chart, registerables } from 'chart.js';
 
-export default {
-  name: "statistics",
-  components: {Icon}
+Chart.register(...registerables);
+
+const lineChartOptions = {
+  type: 'line',
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+    }
+  }
 }
+const lineChartData = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  datasets: [
+    {
+      label: '',
+      data: [0, 800, 1000, 850, 1700, 500, 1300, 800, 650, 1700, 500, 1300],
+      fill: false,
+      borderColor: '#00A0E3',
+      tension: 0
+    },
+  ]
+};
 </script>
-
-<style scoped>
-
+<style>
+canvas {
+  height: 450px !important;
+}
 </style>
